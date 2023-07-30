f59183140e5d7a9ee28fb9553d6c2fa2ba8ebdc7
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2023-07-30 19:50:32 -0300

    feat: routes test/sample csv

---
+++ server/__tests__/csvRoutes.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const app = require('../src/index');
+
+
+describe('Testing routes', () => {
+  test('POST /file - Should return status 200', async () => {
+    const res = await request(app)
+      .post('/file')
+      .attach('csvFile', '../uploads/');
+
+    expect(res.status).toBe(200);
+  });
+
+  test('GET /users - Should return status 200', async () => {
+    const res = await request(app).get('/users');
+
+    expect(res.status).toBe(200);
+  });
+
+  it('Should return status 404 when users are not found', async () => {
+    await CsvData.deleteMany({});
+
+    const res = await request(app).get('/users');
+
+    expect(res.status).toBe(404);
+  });
+
+});
+
+
+
