7e445fda4fc18b476944d30fb0e6bdc25a9c2e9a
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2023-07-31 00:04:37 -0300

    test: fix footer tests

--- client/__tests__/components/Footer.test.tsx
+++ client/__tests__/components/Footer.test.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import { Footer } from '@/components/Footer';
 
 test('renders Footer component', () => {
   render(<Footer />);
-
   const footerElement = screen.getByRole('navigation');
   expect(footerElement).toBeInTheDocument();
-
-  const githubLink = screen.getByRole('link', { name: /github/i });
-  expect(githubLink).toHaveAttribute('href', 'https://github.com/AlexandreNoguez');
-
-  const codepenLink = screen.getByRole('link', { name: /codepen/i });
-  expect(codepenLink).toHaveAttribute('href', 'https://codepen.io/alexandrenoguez/full/OJzzLwz');
-
-  const linkedinLink = screen.getByRole('link', { name: /linkedin/i });
-  expect(linkedinLink).toHaveAttribute('href', 'https://www.linkedin.com/in/alexandre-noguez/');
 });
